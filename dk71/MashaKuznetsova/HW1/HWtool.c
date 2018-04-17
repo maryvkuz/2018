27d7ba8e3273f9b25e8ffd473855717404c1d393
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-04-17 16:47:19 +0300

    Create HWtool.c

---
+++ dk71/MashaKuznetsova/HW1/HWtool.c
@@ -0,0 +1,27 @@
+//
+// Created by vadim on 12.03.2018.
+//
+#include "HWtool.h"
+#include "stdio.h"
+void Search(char *str, char *WhatFind)
+{
+  int CONST =0;
+    int i ,j;
+    int Rezult=0;
+    for(i = 0, j = 0; str[i] !='\0'  ; i++, j++)
+    {
+
+        if(str[i] == WhatFind[j])
+        {
+            CONST ++;
+            if(WhatFind[j+1]== '\0')
+                Rezult++;
+        }
+        else
+        {
+            j = -1;
+        }
+    }
+
+    printf("%d",Rezult);
+}
